303eed5dd06c379895923a4c7df7af1365e45761
tanjaq <tkruglova95@gmail.com>
2023-02-28 20:32:20 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/users', (req, res) => {
+  const { username, password } = req.body
+
+  const validUsername = true
+  const validPassword = true
+
+  if (validUsername && validPassword) {
+    res.send({message: "Valid User"})
+  } else {
+    res.send({error: "Invalid User"})
+  }
+})
+
+
+app.listen(3000, () => console.log("Listening on port 3000"))
