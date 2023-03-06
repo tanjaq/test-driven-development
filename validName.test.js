fd1335d742cfb0b891fcfcbde45ff8b01f4e08d9
ImAMLazy <119529730+ImAMLazy@users.noreply.github.com>
2023-03-06 12:59:07 +0200

    added Name check

--- validName.test.js
+++ validName.test.js
@@ -3,3 +3,15 @@ const validPass = require('./validating/validName.js');
 test("return false for empty value", () => {
     expect(validPass("")).toBe(false);
 });
+
+test("return false if name < 6 chars", () => {
+    expect(validPass("rgre")).toBe(false);
+});
+
+test("return false if name > 30 chars", () => {
+    expect(validPass("fndigirofmbnfjgoekgjreigmejgijeigoejgioelfjfo")).toBe(false);
+});
+
+test("return false if name has symbols instaed characters, numbers and '.'", () => {
+    expect(validPass("fnFigirofm.bn%fjgoekgjreigm")).toBe(false);
+});
--- validating/validName.js
+++ validating/validName.js
@@ -1,8 +1,12 @@
-function validName(pass)
+function validName(name)
 {
-    
+    let moreThan5Chars = name.length > 5;
+    let lessThan31Chars = name.length < 31;
+    let HasNotInvalidSymbols = /[^A-Za-z0-9\.]/g.test(name);
 
-    return ;
+
+
+    return moreThan5Chars && lessThan31Chars && !HasNotInvalidSymbols;
 }
 
-module.exports = validPass;
+module.exports = validName;
