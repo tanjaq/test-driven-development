88dcade0af5ac378825037e681faae800ffec74f
ImAMLazy <119529730+ImAMLazy@users.noreply.github.com>
2023-03-06 13:48:32 +0200

    added eslint, fixed issues of eslint, refactored code with eslint, now have a conflict with eslint and git ( "LF" VS "CRLF" )

--- validating/validName.js
+++ validating/validName.js
@@ -1,12 +1,14 @@
-function validName(name)
-{
-    let moreThan5Chars = name.length > 5;
-    let lessThan31Chars = name.length < 31;
-    let HasNotInvalidSymbols = /[^A-Za-z0-9\.]/g.test(name);
+/**
+ * Validate a password
+ * @param {int} name user name.
+ * @return {bool} return true or false depending on the valid of name.
+ * */
+function validName(name) {
+  const moreThan5Chars = name.length > 5;
+  const lessThan31Chars = name.length < 31;
+  const HasNotInvalidSymbols = /[^A-Za-z0-9\.]/g.test(name);
 
-
-
-    return moreThan5Chars && lessThan31Chars && !HasNotInvalidSymbols;
+  return moreThan5Chars && lessThan31Chars && !HasNotInvalidSymbols;
 }
 
 module.exports = validName;
--- validating/validPass.js
+++ validating/validPass.js
@@ -1,13 +1,18 @@
-function validPass(pass)
-{
-    let moreThan8Chars = pass.length >= 8;
-    let hasNumber = /[0-9]/g.test(pass);
-    let hasLowerCase = /[a-z]/g.test(pass);
-    let hasUpperCase = /[A-Z]/g.test(pass);
-    let hasSpecialChars = /[!-\/:-@[-`{-~]/g.test(pass);
+/**
+ * Validate a password
+ * @param {int} pass user password.
+ * @return {bool} return true or false depending on the valid of pass.
+* */
+function validPass(pass) {
+  const moreThan8Chars = pass.length >= 8;
+  const hasNumber = /[0-9]/g.test(pass);
+  const hasLowerCase = /[a-z]/g.test(pass);
+  const hasUpperCase = /[A-Z]/g.test(pass);
+  const hasSpecialChars = /[!-\/:-@[-`{-~]/g.test(pass);
 
 
-    return moreThan8Chars && hasNumber && hasLowerCase && hasUpperCase && !hasSpecialChars;
+  return moreThan8Chars && hasNumber &&
+   hasLowerCase && hasUpperCase && !hasSpecialChars;
 }
 
 module.exports = validPass;
