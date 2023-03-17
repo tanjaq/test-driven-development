b19622dc8214321cf8c1b67ff5b199d9ae52823c
Jüri Kajava <73351309+kajava-juri@users.noreply.github.com>
2023-03-17 13:57:53 +0200

    username validation mock and tests

--- app.test.js
+++ app.test.js
@@ -1,10 +1,13 @@
 const createApp = require('./app')
 const request = require('supertest')
 const validatePassword = require('./validation/validatePassword')
+const validateUsername = require('./validation/validateUsername')
 
-const app = createApp(validatePassword)
+const app = createApp(validatePassword, validateUsername)
 
 describe('given correct username and password', () => {
+
+
     test('return status 200', async () => {
         const response = await request(app).post('/users').send({
             username: 'Username',
@@ -20,7 +23,25 @@ describe('given correct username and password', () => {
         expect(response.body.userId).toBeDefined()
     })
     // test response content type?? 
+    test('/users endpoint returns json', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Passw0rd123'
+        })
+        const contentType = response.get('Content-Type');
+        expect(contentType).toBeDefined()
+        expect(contentType).toContain("application/json")
+    })
     // test response message
+    test('/users endpoint returns success message', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Passw0rd123'
+        })
+        const message = response.body.message;
+        expect(message).toBeDefined()
+        expect(message).toBe("Valid User")
+    })
 })
 
 describe('given incorrect or missing username and password', () => {
@@ -32,7 +53,26 @@ describe('given incorrect or missing username and password', () => {
         expect(response.statusCode).toBe(400)
     })
     // test response does NOT contain userId
+    test('does not return userId', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'Pass'
+        })
+        expect(response.body.userId).not.toBeDefined()
+    })
     // test response message
+    test('/users endpoint returns error message', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'Pass'
+        })
+        const error = response.body.error;
+        expect(error).toBeDefined()
+        expect(error).toBe("Invalid User")
+    })
+
+    //Aren't the vaidation tests already testing these? And I thought we should not be dependant on the validation service in these test suites
+
     // test missing username
     // test missing password
     // test missing username and password
--- appMock.test.js
+++ appMock.test.js
@@ -4,7 +4,10 @@ const request = require('supertest')
 const mockPasswordValidation = jest.fn();
 mockPasswordValidation.mockImplementation(password => password.length >= 10)
 
-const app = createApp(mockPasswordValidation)
+const mockUsernameValidation = jest.fn();
+mockUsernameValidation.mockImplementation(username => username.length >= 6 && username.length <=30)
+
+const app = createApp(mockPasswordValidation, mockUsernameValidation)
 
 //todo add mock implementation to username
 
@@ -25,7 +28,30 @@ describe('given correct username and password', () => {
         })
         expect(response.body.userId).toBeDefined()
     })
-    // add more tests here
+    // test response content type?? 
+    test('/users endpoint returns json', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Passw0rd123'
+        })
+        const contentType = response.get('Content-Type');
+        expect(contentType).toBeDefined()
+        expect(contentType).toContain("application/json")
+        expect(mockPasswordValidation).toHaveBeenCalled();
+        expect(mockPasswordValidation).toHaveReturnedWith(true);
+    })
+    // test response message
+    test('/users endpoint returns success message', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: 'Passw0rd123'
+        })
+        const message = response.body.message;
+        expect(message).toBeDefined()
+        expect(message).toBe("Valid User")
+        expect(mockPasswordValidation).toHaveBeenCalled();
+        expect(mockPasswordValidation).toHaveReturnedWith(true);
+    })
 })
 
 describe('given incorrect username and password', () => {
@@ -39,4 +65,28 @@ describe('given incorrect username and password', () => {
         expect(mockPasswordValidation).toHaveBeenCalled()
     })
     // add more tests here
+    // test response does NOT contain userId
+    test('does not return userId', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'Pass'
+        })
+        expect(response.body.userId).not.toBeDefined()
+        expect(response.statusCode).toBe(400)
+        expect(mockPasswordValidation).toHaveReturnedWith(false)
+        expect(mockPasswordValidation).toHaveBeenCalled()
+    })
+    // test response message
+    test('/users endpoint returns error message', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'User',
+            password: 'Pass'
+        })
+        const error = response.body.error;
+        expect(error).toBeDefined()
+        expect(error).toBe("Invalid User")
+        expect(response.statusCode).toBe(400)
+        expect(mockPasswordValidation).toHaveReturnedWith(false)
+        expect(mockPasswordValidation).toHaveBeenCalled()
+    })
 })
